1c93f93726c95943599d7dbe64c1a0bcf0fe6ed4
user <user@DESKTOP-SS9RJG3>
2021-06-01 19:19:58 +0300

    Добавьте файлы проекта.

---
+++ StudentRef/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+
+namespace StudentRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly asm = Assembly.LoadFrom(@"D:\FOZZY_GROUP\myPractice\reflectionFirst\reflectionFirst\bin\Debug\reflectionFirst.dll");
+
+            Type t = asm.GetType("reflectionFirst.Student", true, true);
+
+            object obj = Activator.CreateInstance(t, new object[2] {"Leon", 17});
+
+            MethodInfo method = t.GetMethod("ShowResult");
+
+            object result = method.Invoke(obj, new object[] { });
+
+            Console.WriteLine(result);
+
+            object objSecond = Activator.CreateInstance(t, new object[2] { "Kivi", 22 });
+
+            object resultSecond = method.Invoke(objSecond, new object[] { });
+            
+            Console.WriteLine(resultSecond);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ reflectionFirst/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace reflectionFirst
+{
+    public class Student
+    {
+        private string name { get; set; }
+        private int age { get; set; }
+
+        public Student(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public void ShowResult()
+        {
+            if (age >= 18)
+                Console.WriteLine($"{name} - you are adult");
+            else
+                Console.WriteLine($"{name} - you are child");
+        }
+    }
+}
